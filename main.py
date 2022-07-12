ebb52440d406cb3af1cea75db5d8572d88f9737d
imanolarza <imanolarza@gmail.com>
2022-07-11 22:02:39 -0400

    comando uptime

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from discord.ext import commands
 from flask_web import keep_alive
 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import logging
 import os
 
 
 _logger = logging.getLogger(__name__)
 
-bot = commands.Bot(command_prefix='m!', case_insensitive=True)
+bot = commands.Bot(command_prefix='m!!', case_insensitive=True)
 
 # Global Variables (temporal)
 snipe = False
@@ -19,6 +20,9 @@ uptime = False
 @bot.event
 async def on_ready():
     global uptime
+
+    uptime = datetime.today()
+
     print('listo!')
 
 @bot.event
@@ -37,7 +41,27 @@ async def snipe(ctx):
     embed.set_author(name=snipe.author, icon_url=snipe.author.avatar_url)
     embed.timestamp = snipe.created_at
 
-    await ctx.send(embed=embed) 
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def uptime(ctx):
+    today = datetime.now()
+    delta = relativedelta(today, uptime)
+
+    dates = [
+        [delta.years, 'año' if delta.years == 1 else 'años'],
+        [delta.months, 'mes' if delta.months == 1 else 'meses'],
+        [delta.hours, 'día' if delta.hours == 1 else 'horas'],
+        [delta.minutes, 'minuto' if delta.minutes == 1 else 'minutos'],
+        [delta.seconds, 'segundo' if delta.seconds == 1 else 'segundos'],
+    ]
+    res = []
+    for d in dates:
+        if d[0] != 0:
+            res.append('%s %s' % (d[0],d[1]))
+
+    await ctx.send(', '.join(res))
+
 
 if __name__ == "__main__":
     token = os.environ.get('DISCORD_TOKEN')
